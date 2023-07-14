1cda62ea6409e5d8cc7beddc6454fd08e83c4cd3
Dimpho Molepo <dimhpomolepo@gmail.com>
2023-07-14 08:25:02 -0700

    Betty again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 
 /**
  * main - A program that print  with puts function
- * Return (0);
+ * Return: 0 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
 
