8296752e8f22051a97e92239bfd9bdb4bee1195f
Dimpho Molepo <dimhpomolepo@gmail.com>
2023-07-21 07:17:49 -0700

    Prints squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square in the terminal
+ * @size: number of times the character # should be printed
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
