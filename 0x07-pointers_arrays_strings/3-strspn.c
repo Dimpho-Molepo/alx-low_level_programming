2c8d199cfc9b65c82e706d71fdfa387b5405a18e
Dimpho Molepo <dimhpomolepo@gmail.com>
2023-08-01 06:49:17 -0700

    String prefix length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
+ *
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int length = 0;
+	int length_s = strlen(s);
+	int length_accept = strlen(accept);
+
+	for (i = 0; i < length_s; i++)
+	{
+		for (j = 0; j < length_accept; j++)
+		{
+			if (accept[j] == s[i])
+				break;
+		}
+
+		if  (accept[j] != s[i])
+			break;
+		else 
+			length++;
+	}
+	return (length);
+}
